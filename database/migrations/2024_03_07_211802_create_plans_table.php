aa36a1c3d70b3d4d8c83fe21ee6c2a3dee67ae17
Muhsin Azmal <muhsinstudent12@gmail.com>
2024-03-08 04:00:45 +0600

    created migration for plan

--- database/migrations/2024_03_07_211802_create_plans_table.php
+++ database/migrations/2024_03_07_211802_create_plans_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('plans', function (Blueprint $table) {
             $table->id();
+            $table->string('plan_name');
+            $table->text('plan_description')->nullable();
+            $table->float('plan_price');
             $table->timestamps();
         });
     }
